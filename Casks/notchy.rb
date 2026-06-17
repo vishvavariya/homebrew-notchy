0757ed7318478f82ea9458c9269a448ba4294f97
Vishva Variya <variyavishva98@gmail.com>
2026-06-17 17:18:46 +0530

    Bump Notchy to 1.0.29

--- Casks/notchy.rb
+++ Casks/notchy.rb
@@ -1,6 +1,6 @@
 cask "notchy" do
-  version "1.0.28"
-  sha256 "c42f53ea94a431ac0ab910a0f3a93f2f0592082633860ce4a5efea9e7b49e128"
+  version "1.0.29"
+  sha256 "41b8886ca24256f6a5cc4b2e0e5ec75169c2feaa20f2c8128f4a21a0e170eac2"
 
   url "https://notchy.dev/Notchy-#{version}.zip",
       verified: "notchy.dev/"
