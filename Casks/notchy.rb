30e44aaba751a0735c0b6562490c5c108858f289
Vishva Variya <variyavishva98@gmail.com>
2026-08-14 09:32:40 +0200

    Bump Notchy to 1.0.112

--- Casks/notchy.rb
+++ Casks/notchy.rb
@@ -1,6 +1,6 @@
 cask "notchy" do
-  version "1.0.111"
-  sha256 "5277f6fa897345cf9e047af26d48dc06ec34319184df462e249a9ac64839ef47"
+  version "1.0.112"
+  sha256 "34ed74cd90878e0f9d8f6b35778e25566b000854f5a22d9fa3f030a6a691a9ea"
 
   url "https://github.com/vishvavariya/notchy-feedback/releases/download/v#{version}/Notchy-#{version}.zip",
       verified: "github.com/vishvavariya/notchy-feedback/"
